c7e2f3aa3aeca19d84cfc7ece0aa2d147ac75579
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-05 15:43:07 +0530

    1.6 Added User Services

---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+const {
+    updateUser,
+    deleteUser,
+    getAllUsers,
+    getUser,
+} = require("../services/userService");
+require("dotenv").config();
+const _ = require("lodash");
+
+exports.updateUser = async(req, res, next) => {
+    try {
+        const userData = _.pick(req.body, ["username", "mobile", "email"]);
+        userData.userId = req.params.id;
+        // console.log(userData);
+
+        const user = await updateUser(userData);
+        if (user) {
+            return res.status(200).json({
+                stauts: true,
+                message: "User has been updated successfully",
+                user,
+            });
+        } else {
+            throw CreateError(400, "User not found");
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.deleteUser = async(req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const user = await deleteUser({ userId });
+        if (user) {
+            return res.status(200).json({
+                stauts: true,
+                message: "User has been deleted successfully",
+                user,
+            });
+        } else {
+            throw CreateError(400, "User not found");
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.getUser = async(req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const user = await getUser({ userId });
+        if (user) {
+            return res.status(200).json({
+                stauts: true,
+                message: "User has been found successfully",
+                user,
+            });
+        } else {
+            throw CreateError(400, "User not found");
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.getAllUsers = async(req, res, next) => {
+    try {
+        const users = await getAllUsers();
+        if (users) {
+            return res.status(200).json({
+                stauts: true,
+                message: "Users has been found successfully",
+                users,
+            });
+        } else {
+            throw CreateError(400, "Users not found");
+        }
+    } catch (err) {
+        next(err);
+    }
+};
